2020b6f6855ca06b5502aff6b3774c89402d7c32
J01Gonza <juanp01.gh@gmail.com>
2023-05-01 19:15:00 -0600

    Ya casi terminamos

--- proyecto-ia/src/main/java/gt/url/edu/App.java
+++ proyecto-ia/src/main/java/gt/url/edu/App.java
@@ -1,19 +1,71 @@
 package gt.url.edu;
 
-import com.google.firebase.database.*;
-
-import java.io.IOException;
+import java.text.Normalizer;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-
-
+public class App {
+    public static void main(String[] args) {
         Reader reader = new Reader();
-        reader.training("C:\\Repos\\util\\ejemplopalabras.txt");
+        HashMap<String, List<Word>> dataSet = new HashMap<String, List<Word>>();
+        dataSet = reader.training(
+                "C:\\Users\\Juan González\\Documents\\IA\\proyectoIA\\proyecto-ia\\src\\main\\resources\\ejemplopalabras.txt",
+                dataSet);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("exit")) {
+            System.out.print("Por favor ingrese la cadena >");
+            input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            removePunctuation(input);
+            var normalized_string = Normalizer.normalize(input, Normalizer.Form.NFD)
+                    .replaceAll("[^\\p{ASCII}]", "").toLowerCase().trim().split("\s");
+            var trueEtiqueta = "";
+            var falseEtiqueta = "";
+            Double trueProbabilidad = 0.0;
+            Double falseProbabilidad = 1.0;
 
-
+            // P (youlo | ham) = (1 + 1)/ (7 + 15)
+            for (var etiqueta : dataSet.entrySet()) {
+                falseProbabilidad = 1.0;
+                for (var texto : normalized_string) {
+                    falseEtiqueta = etiqueta.getKey();
+                    Word valor = null;
+                    Optional<Word> stream = null;
+                    stream = etiqueta.getValue().stream().filter(x -> x.value.equals(texto)).findFirst();
+                    if (!stream.isEmpty()) {
+                        valor = stream.get();
+                        Double x = Double.parseDouble(valor.count.toString());
+                        Double y = Double.parseDouble(String.valueOf(etiqueta.getValue().size()));
+                        Double n = Double.parseDouble(reader.getPalabras().toString());
+                        falseProbabilidad *= (x + 1) / (y + n);
+                    } else {
+                        Double y = Double.parseDouble(String.valueOf(etiqueta.getValue().size()));
+                        Double n = Double.parseDouble(reader.getPalabras().toString());
+                        falseProbabilidad *= (0 + 1) / (y + n);
+                    }
+                }
+                if (trueProbabilidad < falseProbabilidad) {
+                    trueEtiqueta = falseEtiqueta;
+                    trueProbabilidad = falseProbabilidad;
+                }
+            }
+            System.out.println(input + " pertenece a " + trueEtiqueta + " con una probabilidad de " + trueProbabilidad);
+        }
+        scanner.close();
     }
+
+    static String removePunctuation(String s) {
+        StringBuilder res = new StringBuilder();
+        for (Character c : s.toCharArray()) {
+            if(Character.isLetterOrDigit(c))
+                res.append(c);
+        }
+        return res.toString();
+    }
+
 }
--- proyecto-ia/src/main/java/gt/url/edu/Reader.java
+++ proyecto-ia/src/main/java/gt/url/edu/Reader.java
@@ -1,65 +1,66 @@
 package gt.url.edu;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.NotNull;
 
-import javax.security.auth.callback.Callback;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-
 
 public class Reader {
 
-    public HashMap<String, List<Word>> training(String path){
+    private Integer Palabras;
 
-        HashMap<String, List<Word>> words = new HashMap<String,List<Word> >();
+    public Integer getPalabras() {
+        return Palabras;
+    }
+
+    static String removePunctuation(String s) {
+        StringBuilder res = new StringBuilder();
+        for (Character c : s.toCharArray()) {
+            if(Character.isLetterOrDigit(c))
+                res.append(c);
+        }
+        return res.toString();
+    }
+
+    public HashMap<String, List<Word>> training(String path, HashMap<String, List<Word>> words) {
+        HashSet<String> hashSet = new HashSet<String>();
         BufferedReader reader = null;
         try {
-            
+
             reader = new BufferedReader(new FileReader(path));
             String line;
             while ((line = reader.readLine()) != null) {
+                removePunctuation(line);
                 var linea = line.split("\\|");
 
+                var normalized_string = Normalizer.normalize(linea[0], Normalizer.Form.NFD)
+                        .replaceAll("[^\\p{ASCII}]", "").toLowerCase().trim();
+                var normalized_tag = Normalizer.normalize(linea[1], Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "")
+                        .toLowerCase().trim();
 
-                var normalized_string = Normalizer.
-                        normalize(linea[0], Normalizer.Form.NFD).
-                        replaceAll("[^\\p{ASCII}]|\\,|\\.|\"", "").
-                        toLowerCase().trim();
-                var normalized_tag = Normalizer.
-                        normalize(linea[1], Normalizer.Form.NFD).
-                        replaceAll("[^\\p{ASCII}]", "").
-                        toLowerCase().trim();
-
-//                System.out.println(normalized_string);
-//                System.out.println(normalized_tag);
                 var splitted = normalized_string.split("\s");
-                //var tags = fireBaseService.db.getReference("etiqueta");
                 List<Word> wordList = new ArrayList<>();
-                for (var word: splitted ) {
-                    if(words.containsKey(normalized_tag)){
+                for (var word : splitted) {
+                    if (words.containsKey(normalized_tag)) {
                         wordList = words.get(normalized_tag);
                     }
                     var existe = false;
-                    for (var w: wordList ) {
-                        if(w.value.equals(word)){
+                    for (var w : wordList) {
+                        if (w.value.equals(word)) {
                             w.count++;
                             existe = true;
                         }
                     }
-                    if(!existe){
-                        wordList.add(new Word(normalized_tag,word,1));
+                    if (!existe) {
+                        wordList.add(new Word(normalized_tag, word, 1));
+                        hashSet.add(word);
                     }
                 }
                 words.put(normalized_tag, wordList);
-
             }
             System.out.println("Aprendizaje finalizado");
         } catch (IOException e) {
@@ -73,8 +74,7 @@ public class Reader {
                 e.printStackTrace();
             }
         }
+        Palabras = hashSet.size();
         return words;
     }
-
-
 }
